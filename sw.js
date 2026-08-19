c33e439701601e527a4d3c36289042f99c0bf65f
kl0ckhart <toddle_capsize3h@icloud.com>
2026-08-19 17:42:09 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "cc-ramp-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
+});
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); })); }).then(function(){ return self.clients.claim(); }));
+});
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(function(resp){
+      var copy = resp.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+      return resp;
+    }).catch(function(){
+      return caches.match(e.request).then(function(r){ return r || caches.match("./index.html"); });
+    })
+  );
+});
